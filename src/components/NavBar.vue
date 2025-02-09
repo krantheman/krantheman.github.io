fbf4eefee4fa1bbec7a594e8282e55e4c66bf701
krantheman <akash.akasht.tom@gmail.com>
2025-02-09 23:17:28 +0530

    add socials

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,27 +1,52 @@
 <script setup>
-import { RouterLink, RouterView, useRoute } from "vue-router";
+import { RouterLink, useRoute } from "vue-router";
+import Github from "./icons/Github.vue";
+import Instagram from "./icons/Instagram.vue";
+import Linkedin from "./icons/Linkedin.vue";
+import Telegram from "./icons/Telegram.vue";
+import Email from "./icons/Email.vue";
 
 const route = useRoute();
 
-const links = [
+const websiteLinks = [
 	{ name: "Home", path: "/" },
 	{ name: "Blogs", path: "/blogs" },
 	{ name: "Gallery", path: "/gallery" },
 	{ name: "About", path: "/about" },
 ];
+
+const socialLinks = [
+	{ icon: Github, href: "https://github.com/krantheman" },
+	{ icon: Linkedin, href: "https://www.linkedin.com/in/akash-tom/" },
+	{ icon: Instagram, href: "https://www.instagram.com/monsieur_le_tom/" },
+	{ icon: Telegram, href: "https://t.me/akash_tom" },
+	{ icon: Email, href: "mailto:me@akashtom.com" },
+];
 </script>
 
 <template>
 	<header>
-		<nav class="flex justify-between">
+		<nav class="flex space-x-8 items-center">
 			<RouterLink
-				v-for="link in links"
+				v-for="link in websiteLinks"
 				:to="link.path"
-				class="hover:text-sky-200"
+				class="hover:text-sky-200 font-medium text-lg"
 				:class="{ 'text-sky-200': route.name === link.name }"
 			>
 				{{ link.name }}
 			</RouterLink>
+			<div class="mx-auto" />
+			<a
+				v-for="link in socialLinks"
+				:href="link.href"
+				target="_blank"
+				rel="noopener"
+			>
+				<component
+					:is="link.icon"
+					class="h-5 w-5 fill-gray-200 hover:fill-sky-200"
+				/>
+			</a>
 		</nav>
 	</header>
 </template>
