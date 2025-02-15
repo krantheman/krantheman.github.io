c68460d7954781848c28eaa1616e74742f5be593
krantheman <akash.akasht.tom@gmail.com>
2025-02-15 14:40:13 +0530

    redirect on invalid blog path

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,15 +1,19 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
 import markdownIt from "markdown-it";
 
 const route = useRoute();
-const filePath = `/blogs/${route.path.split("/").at(-1)}.md`;
-const htmlContent = ref();
+const router = useRouter();
+const md = markdownIt();
+
+const htmlContent = ref("");
 
 onMounted(async () => {
+	const filePath = `/blogs/${route.path.split("/").at(-1)}.md`;
 	const fileContent = await fetch(filePath).then((res) => res.text());
-	htmlContent.value = markdownIt().render(fileContent);
+	if (fileContent.includes("<!doctype html>")) router.replace("/blogs");
+	htmlContent.value = md.render(fileContent);
 });
 </script>
 
