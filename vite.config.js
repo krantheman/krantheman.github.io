0e3fed9c390f051044acfa45ace02ada493faefe
krantheman <akash.akasht.tom@gmail.com>
2025-04-01 00:23:09 +0530

    add 404

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
+import { resolve } from "path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -6,11 +7,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "./",
 	plugins: [vue(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				404: resolve(__dirname, "public/404.html"),
+			},
 		},
 	},
 });
